41d8bc5a200c9c9664aed788d93e52c4fceaed39
rbss420 <76229135+rbss420@users.noreply.github.com>
2020-12-31 00:38:14 +0800

    Add files via upload

---
+++ scr/help.js
@@ -0,0 +1,165 @@
+const help = (prefix) => { 
+	return `[ 𝐌𝐄𝐍𝐔 𝐓𝟎𝟐𝟕𝐁𝐎𝐓 ]                  
+
+┏━━━━°❀ ❬ 𝘼𝘽𝙊𝙐𝙏 ❭ ❀°━━━━┓
+┃╔══════════════════╗
+┏❉ *${prefix}info*
+┣❉ *${prefix}creator*
+┣❉ *${prefix}donasi*
+┗❉ *${prefix}report* [lapor bug]
+┃╚══════════════════╝
+┣━━━━°❀ ❬ 𝙊𝙒𝙉𝙀𝙍 ❭ ❀°━━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}bc*
+┃╠➥ *${prefix}block*
+┃╠➥ *${prefix}unblock*
+┃╚══════════════════╝
+┣━━━°❀ ❬ 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 ❭ ❀°━━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}sticker*
+┃╠➥ *${prefix}tsticker* [teks]
+┃╠➥ *${prefix}nulis* [teks]
+┃╚══════════════════╝
+┣━━━°❀ ❬ 𝙐𝙋𝘿𝘼𝙏𝙀 ❭ ❀°━━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}toimg*
+┃╠➥ *${prefix}hobby* [Teks]
+┃╠➥ *${prefix}apakah* [Teks]
+┃╠➥ *${prefix}bisakah* [Teks]
+┃╠➥ *${prefix}kapankah* [Teks]
+┃╠➥ *${prefix}watak* [Teks]
+┃╠➥ *${prefix}rate* [Teks]
+┃╠➥ *${prefix}truth*
+┃╠➥ *${prefix}dare*
+┃╠➥ *${prefix}testime*
+┃╠➥ *${prefix}meme*
+┃╠➥ *${prefix}ramaljadian*
+┃╠➥ *${prefix}persengay*
+┃╠➥ *${prefix}cerpen*
+┃╠➥ *${prefix}quotes*
+┃╠➥ *${prefix}chord* [nama lagu]
+┃╠➥ *${prefix}lirik* [nama lagu]
+┃╠➥ *${prefix}bucin*
+┃╠➥ *${prefix}walpaperhd*
+┃╠➥ *${prefix}pokemon*
+┃╠➥ *${prefix}inu*
+┃╠➥ *${prefix}elang*
+┃╠➥ *${prefix}wiki*
+┃╠➥ *${prefix}brainly*
+┃╠➥ *${prefix}image*
+┃╠➥ *${prefix}infocuaca* [daerah]
+┃╠➥ *${prefix}infogempa*
+┃╠➥ *${prefix}infonomor* [62xxx]
+┃╠➥ *${prefix}infomobil*
+┃╠➥ *${prefix}infomotor*
+┃╠➥ *${prefix}checkjodoh*
+┃╠➥ *${prefix}shorturl*
+┃╠➥ *${prefix}quotesmaker*
+┃╠➥ *${prefix}joox*
+┃╠➥ *${prefix}randomcat*
+┃╠➥ *${prefix}mlherolist*
+┃╠➥ *${prefix}say* [teks]
+┃╠➥ *${prefix}resepmakanan*
+┃╠➥ *${prefix}indohot*
+┃╠➥ *${prefix}game*
+┃╠➥ *${prefix}tts* [id] [teks]
+┃╠➥ *${prefix}ttp* [teks]
+┃╠➥ *${prefix}galaxytext* [teks]
+┃╠➥ *${prefix}pornlogo* [teks|teks]
+┃╠➥ *${prefix}glitch* [teks|teks]
+┃╠➥ *${prefix}wolflogo* [teks|teks]
+┃╠➥ *${prefix}lionlogo* [teks|teks]
+┃╠➥ *${prefix}ninjalogo* [teks|teks]
+┃╠➥ *${prefix}text3d* [teks]
+┃╠➥ *${prefix}artinama* [nama]
+┃╠➥ *${prefix}ramalhp* [nomor]
+┃╠➥ *${prefix}ssweb* [linkweb]
+┃╠➥ *${prefix}map* [daerah]
+┃╠➥ *${prefix}url2img* [link]
+┃╚══════════════════╝
+┣━━━━°❀ ❬ 𝙉𝙎𝙁𝙒 ❭ ❀°━━━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}anime*
+┃╠➥ *${prefix}loli*
+┃╠➥ *${prefix}waifu*
+┃╠➥ *${prefix}waifu2*
+┃╠➥ *${prefix}nekonime*
+┃╠➥ *${prefix}wibu*
+┃╠➥ *${prefix}randomanime*
+┃╠➥ *${prefix}randomhentai*
+┃╠➥ *${prefix}nsfwtrap*
+┃╠➥ *${prefix}nsfwloli*
+┃╠➥ *${prefix}nsfwneko*
+┃╚══════════════════╝
+┣━━°❀ ❬ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 ❭ ❀°━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}ytsearch* [search]
+┃╠➥ *${prefix}ytmp3* [link]
+┃╠➥ *${prefix}tiktok* [link]
+┃╚══════════════════╝
+┣━━°❀ ❬ 𝙂𝙍𝙊𝙐𝙋 𝙊𝙉𝙇𝙔 ❭ ❀°━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}listadmins*
+┃╠➥ *${prefix}infogc*
+┃╠➥ *${prefix}linkgc*
+┃╠➥ *${prefix}add* [62xxx]
+┃╠➥ *${prefix}kick* [tag]
+┃╠➥ *${prefix}setpp*
+┃╠➥ *${prefix}promote* [tag]
+┃╠➥ *${prefix}unpromote* [tag]
+┃╠➥ *${prefix}delete*
+┃╠➥ *${prefix}hidetag*
+┃╠➥ *${prefix}tagall*
+┃╠➥ *${prefix}tagme*
+┃╠➥ *${prefix}setdesc* [teks]
+┃╠➥ *${prefix}setname* [teks]
+┃╠➥ *${prefix}setpp*
+┃╠➥ *${prefix}groupinfo*
+┃╠➥ *${prefix}group* [buka/tutup]
+┃╠➥ *${prefix}welcome* [1/0]
+┃╠➥ *${prefix}nsfw* [1/0]
+┃╠➥ *${prefix}simih* [1/0]
+┃╠➥ *${prefix}openanime* [1/0]
+┃╠➥ *${prefix}naruto*
+┃╠➥ *${prefix}boruto*
+┃╠➥ *${prefix}minato*
+┃╠➥ *${prefix}hinata*
+┃╠➥ *${prefix}sasuke*
+┃╠➥ *${prefix}sakura*
+┃╚══════════════════╝
+┣━━━━°❀ ❬ 𝙊𝙒𝙉𝙀𝙍 ❭ ❀°━━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}bc* 
+┃╠➥ *${prefix}leave*
+┃╠➥ *${prefix}clearall*
+┃╠➥ *${prefix}setprefix*
+┃╠➥ *${prefix}clone*
+┃╠➥ *${prefix}block*
+┃╠➥ *${prefix}unblock*
+┃╠➥ *${prefix}getses*
+┃╚══════════════════╝
+┣━━━━°❀ ❬ 𝙊𝙏𝙃𝙀𝙍 ❭ ❀°━━━━⊱
+┃╔══════════════════╗
+┃╠➥ *${prefix}ytsearch*
+┃╠➥ *${prefix}speed*
+┃╠➥ *${prefix}blocklist*
+┃╠➥ *${prefix}simi*
+┃╠➥ *${prefix}wait*
+┃╠➥ *${prefix}fitnah*
+┃╠➥ *${prefix}fototiktok*
+┃╠➥ *${prefix}tiktok*
+┃╠➥ *${prefix}tiktokstalk*
+┃╠➥ *${prefix}igstalk*
+┃╠➥ *${prefix}url2img*
+┃╚══════════════════╝
+┣━━━━━━━━━━━━━━━━━━━━
+┃ 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗧𝟬𝟮𝟳𝗕𝗢𝗧
+┗━━━━━━━━━━━━━━━━━━━━
+===========================
+===========================
+𝗚𝗥𝗢𝗨𝗣 𝗕𝗢𝗧
+'https://chat.whatsapp.com/L0gow3a9HUoHdwy03uvE7w'
+===========================
+===========================`
+}
+exports.help = help
